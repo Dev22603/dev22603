4be1212b00dbcd3af4060c1f3205a8aa5d870532
Dev Bachani <dev22603@gmail.com>
2025-08-22 15:26:28 +0530

    dotted BG added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	server: {
-		host: "127.0.0.1",
-		port: 5173 // optional, defaults to 5173 anyway
-	}
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@/components": path.resolve(__dirname, "./src/components"),
+      "@/lib": path.resolve(__dirname, "./src/lib"),
+      "@/pages": path.resolve(__dirname, "./src/pages"),
+    },
+  },
+  server: {
+    host: "127.0.0.1",
+    port: 5173, // optional, defaults to 5173 anyway
+  },
 });
