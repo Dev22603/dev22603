70937dfd3dcd0126d0266d827d9899a269beb27d
Dev Bachani <dev22603@gmail.com>
2025-08-22 15:26:28 +0530

    dotted BG added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,33 @@
-function App() {
+import React from "react";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import Home from "@/pages/Home";
+// import Experience from "@/pages/Experience";
+// import Projects from "@/pages/Projects";
+// import Contact from "@/pages/Contact";
+// import GuestBook from "@/pages/GuestBook";
+import { Layout } from "@/components/Layout";
+
+export default function App() {
+    const DARK_BG = "#0d0b0f";
+
 	return (
-		<>
-			<h1 className="text-2xl text-amber-500">Dev</h1>
-		</>
+		<BrowserRouter>
+			<Layout
+				dotColor={{ light: "#d4d4d4", dark: "#404040" }}
+				dotSize={1}
+				dotSpacing={20}
+				fadeToColor={DARK_BG}
+				fadePercentage={10}
+			>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					{/* <Route path="/experience" element={<Experience />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/guestbook" element={<GuestBook/>} /> */}
+					<Route path="*" element={<Navigate to="/" replace />} />
+				</Routes>
+			</Layout>
+		</BrowserRouter>
 	);
 }
-
-export default App;
