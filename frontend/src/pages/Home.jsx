35edbc64befa9707a97d69b51f910017851d9415
Dev Bachani <dev22603@gmail.com>
2025-08-26 17:30:21 +0530

    clean implementation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,14 +14,12 @@ const Home = () => {
 						<img
 							src={banner}
 							alt=""
-							className="rounded-md w-full h-auto bg-white relative z-10"
-							style={{ display: "block", background: "white" }}
+							className="rounded-md w-full h-auto"
 						/>
 						<img
 							src={headshot}
 							alt=""
-							className="rounded-full w-60 h-60 border-6 border-neutral-950 -mt-30 ml-10 bg-white relative z-20"
-							style={{ display: "block", background: "white" }}
+							className="rounded-full w-60 h-60 border-6 border-neutral-950 -mt-30 ml-10 inline-block"
 						/>
 					</div>
 				</div>
